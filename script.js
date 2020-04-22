e18a3805caac6479c292db1219c7fbd7ee0ffa7d
masoncool877 <31975189+masoncool877@users.noreply.github.com>
2020-04-21 23:11:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-setTimeout(function(){console.clear();console.log("%c Copyright 2020 Mason Foy & Isaac Lloyd", "color: white; font-size: 40px; text-shadow: 1px 1px black, -1px -1px black, -1px 1px black, 1px -1px black;");
-console.log("%c email 24lloydisah@washk12.org for details.", "color: black; font-size: 10px;");
-console.log("Don't know why you're here? Press F12 to leave.");
-console.log("Know exactly what you're doing here? Come code with us! https://pvms.glitch.me/Contact?job");}, 2000);
-
-//redirect to https://
-if(location.protocol!='https:'){location.href='https:'+window.location.href.substring(window.location.protocol.length);}
-
 document.onkeydown = keydown;
 
 function keydown(evt){
@@ -13,7 +5,7 @@ function keydown(evt){
   if (evt.ctrlKey && evt.keyCode==68){ //CTRL+ALT+F4
     evt.preventDefault();
     var iframe = document.createElement('iframe');
-    iframe.src = 'https://sketch.io/sketchpad/';
+    iframe.src = 'hide.html';
     iframe.style.position = "fixed";
     iframe.style.width = "100vw";
     iframe.style.height = "100vh";
